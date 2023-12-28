c39d3f9f44a29f90e2a1eb3b6a8c7e92e03c47bd
xrafaelmuller <113266933+xrafaelmuller@users.noreply.github.com>
2023-12-27 23:13:01 -0300

    commit

--- emailstd.py
+++
@@ -1,2 +0,0 @@
-from stdapp import entry_change, messagebox,entry_sender, win32
-
--- stdapp.py
+++ stdapp.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 import win32
-import emailstd
 from tkinter import ttk, scrolledtext
 from tkcalendar import DateEntry
 from tkinter import messagebox
 import win32com.client  # Import win32com.client for Outloo
+
 
 
 # Clipboard enablement
@@ -46,6 +46,48 @@ def create_short(parent):
     entry.pack(fill="x", padx=10, pady=(0, 10))
     return entry
 
+# Main Window
+window = tk.Tk()
+window.geometry("800x700")
+window.title("Standard Change - Lazy Manager")
+
+# Main Label
+label = tk.Label(window, text="Welcome to Lazy Manager for Standard Changes!", font=("Arial", 12))
+label.pack(pady=10)
+
+# Create a frame to group related fields
+frame_request_info = ttk.LabelFrame(window, text="Request Information", padding=(5, 5))
+frame_request_info.pack(padx=5, pady=5, fill="both", expand=False)
+
+# Input Fields
+create_label(frame_request_info, "Change Coordinator Email:")
+entry_sender = create_entry(frame_request_info, width=30)
+
+create_label(frame_request_info, "Request Item Number:")
+entry_ritm = create_entry(frame_request_info, width=30)
+
+# Create a frame to group related fields
+frame_request_details = ttk.LabelFrame(window, text="Request Details", padding=(10, 5), height=100)
+frame_request_details.pack(padx=5, pady=5, fill="both", expand=False)
+
+create_label(frame_request_details, "Standard Activity:")
+entry_short_description = create_short(frame_request_details)
+
+create_label(frame_request_details, "Category:")
+entry_category_options = ["Application - Code", "Application - Configuration", "Application & Database - Code",
+                           "Application & Database - Configuration", "Database", "Database - Code", "Database - Configuration",
+                           "Facilities - Building", "Facilities - Data Center", "Middleware", "Network", "Server", "Security",
+                           "Storage", "Voice / Telecom"]
+dropdown = create_category(frame_request_details, entry_category_options)
+
+
+create_label(frame_request_details, "CAB Approval Date:")
+date_entry = create_date_entry(frame_request_details)
+
+# Create a frame for the text box
+frame_text_box = ttk.LabelFrame(window, text="Configuration Items", padding=(10, 5))
+frame_text_box.pack(padx=5, pady=5, fill="both", expand=False)
+
 def send_email():
     mail_sender = entry_sender.get()
     if not mail_sender:
@@ -59,8 +101,9 @@ def send_email():
     subject_mail = f"{request_item_number} - Review"
     body_mail = update_body_mail_email()
     body_mail = body_mail.replace("RITMXXXXXXX", request_item_number)
-    body_mail = body_mail.replace("Change Coordinator", " ".join([name.capitalize() for name in c_coordinator]))
-
+    body_mail = body_mail.replace("Change_Coordinator", " ".join([name.capitalize() for name in c_coordinator]))
+    body_mail = body_mail.replace("XXSTDTYPEXX",  '\n'.join(entry_category_options))
+                                    
     try:
         outlook = win32com.client.Dispatch('Outlook.Application')
         namespace = outlook.GetNamespace("MAPI")
@@ -98,8 +141,8 @@ def update_body_mail_email():
                                 <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
                                     <p style="font-size: 16px; color: #666666;">Dear Change_Coordinator,</p>
                                     <p style="font-size: 16px; color: #666666;">Your request RITMXXXXXXX for a new Standard Change, XXActivityXX was approved by CAB as a Standard Change on XXXDATEXXX</p>
-                                    <p id="paragrafo">Documents</p>
-                                    <p id="paragrafo-invisivel2" style="display: none;">Descriptions</p>
+                                    <p style="font-size: 16px; color: #666666;">Please refer to KB0912448: How To: Submit a Standard Change / Standard Change Job Aid for information on how to use your new Standard Change. Use the below information to locate your Standard Change in the Catalog</p>
+                                    <li style="font-size: 16px; color: #666666;">Standard Change type: XXSTDTYPEXX</li>
                                     <p id="paragrafo-invisivel3" style="display: none;">Configuration_item</p>
                                     <p id="paragrafo-invisivel4" style="display: none;">Category</p>
                                     <p style="font-size: 16px; color: #666666;">Best regards,<br>IT-Change-Managers@dell.com</p>
@@ -110,48 +153,6 @@ def update_body_mail_email():
                             </html>
     '''    
     return body_mail
-
-
-# Main Window
-window = tk.Tk()
-window.geometry("800x700")
-window.title("Standard Change - Lazy Manager")
-
-# Main Label
-label = tk.Label(window, text="Welcome to Lazy Manager for Standard Changes!", font=("Arial", 12))
-label.pack(pady=10)
-
-# Create a frame to group related fields
-frame_request_info = ttk.LabelFrame(window, text="Request Information", padding=(5, 5))
-frame_request_info.pack(padx=5, pady=5, fill="both", expand=False)
-
-# Input Fields
-create_label(frame_request_info, "Change Coordinator Email:")
-entry_sender = create_entry(frame_request_info, width=30)
-
-create_label(frame_request_info, "Request Item Number:")
-entry_ritm = create_entry(frame_request_info, width=30)
-
-# Create a frame to group related fields
-frame_request_details = ttk.LabelFrame(window, text="Request Details", padding=(10, 5), height=100)
-frame_request_details.pack(padx=5, pady=5, fill="both", expand=False)
-
-create_label(frame_request_details, "Standard Activity:")
-entry_short_description = create_short(frame_request_details)
-
-create_label(frame_request_details, "Category:")
-entry_category_options = ["Application - Code", "Application - Configuration", "Application & Database - Code",
-                           "Application & Database - Configuration", "Database", "Database - Code", "Database - Configuration",
-                           "Facilities - Building", "Facilities - Data Center", "Middleware", "Network", "Server", "Security",
-                           "Storage", "Voice / Telecom"]
-dropdown = create_category(frame_request_details, entry_category_options)
-
-create_label(frame_request_details, "CAB Approval Date:")
-date_entry = create_date_entry(frame_request_details)
-
-# Create a frame for the text box
-frame_text_box = ttk.LabelFrame(window, text="Configuration Items", padding=(10, 5))
-frame_text_box.pack(padx=5, pady=5, fill="both", expand=False)
 
 # Create a scrolled text widget
 text_box = scrolledtext.ScrolledText(frame_text_box, wrap=tk.WORD, width=40, height=10, font=("Arial", 10))
